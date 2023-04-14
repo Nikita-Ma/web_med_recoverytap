aadfe4681c3d2b2a059f534af6559f31de7c5aae
Daria <dariachugunova2003@gmail.com>
2023-04-14 22:04:58 +0300

    Delete js directory

--- js/darkmode.js
+++
@@ -1,8 +0,0 @@
-// переключение на темную тему
-
-const themeSwitcher = document.querySelector('.theme-switcher');
-const body = document.querySelector('body');
-
-themeSwitcher.addEventListener('click', () => {
-  body.classList.toggle('dark-theme');
-});
